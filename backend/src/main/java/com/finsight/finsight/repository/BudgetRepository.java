3e0845a2a537c2511411371d14143d48819b96b3
ravindar <ravindardeeti799@gmail.com>
2025-12-03 19:15:09 +0530

    added repo layer

---
+++ backend/src/main/java/com/finsight/finsight/repository/BudgetRepository.java
@@ -0,0 +1,11 @@
+package com.finsight.finsight.repository;
+
+import com.finsight.finsight.entity.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BudgetRepository extends JpaRepository<Budget, Long> {
+}
---
+++ backend/src/main/java/com/finsight/finsight/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.finsight.finsight.repository;
+
+import com.finsight.finsight.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    boolean findByEmail(String Email);
+
+    boolean existsByEmail(String email);
+}
